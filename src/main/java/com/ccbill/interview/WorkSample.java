f0c68c3fe27ef378cc40312ef109971994e4db02
Robert Novak <me@robert-novak.com>
2016-09-21 14:06:55 -0700

    added testing, comments

--- src/main/java/com/ccbill/interview/WorkSample.java
+++ src/main/java/com/ccbill/interview/WorkSample.java
@@ -2,11 +2,105 @@ package com.ccbill.interview;
 
 import org.apache.log4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+
 public class WorkSample {
 
     private static final Logger logger = Logger.getLogger(WorkSample.class);
 
+    protected class Data {
+        int N;
+        int K;
+        int arrival_times[];
+    }
+
+
+    protected Data parse(String input) {
+
+        Data result = null;
+
+        assert(input != null);
+
+        String[] lines = input.split("\\n");
+
+        assert(lines.length == 2);
+
+        String[] fields = lines[0].split(" ");
+
+        assert(fields.length == 2); // Expects only N and K to be on the first length
+
+        result = new Data();
+
+        result.N = Integer.parseInt(fields[0]);
+        result.K = Integer.parseInt(fields[1]);
+
+        String[] data = lines[1].split(" ");
+
+        assert(data.length == result.N);    // Expect <N> arrival times
+
+        result.arrival_times = new int[result.N];
+
+        for (int index = 0; index < result.N; index++)
+            result.arrival_times[index] = Integer.parseInt(data[index]);
+
+
+        return result;
+    }
+
+    protected String solve(Data data) {
+        String result = "YES";
+
+            // Simle sanity check
+        assert (data != null && data.N >= 0 && data.K <= data.N);
+
+        int count = 0;
+
+        for (int index = 0; index < data.N; index++)
+            if (data.arrival_times[index] <= 0)
+                count += 1;
+
+        if (count >= data.K)
+            result = "NO";
+
+        return result;
+    }
+
+    /**
+     * General Procedure below:
+     *  T = readLine()
+     *  for i in (1..T)
+     *      data = parse(input)
+     *      solution = solve(data)
+     *      print (solution)
+     * @param args - Command Line arguments (expects none)
+     */
     public static void main (String[] args) {
+
+        WorkSample sample = new WorkSample();
+
+        try (InputStreamReader isr = new InputStreamReader(System.in);
+                BufferedReader reader = new BufferedReader(isr)) {
+
+            Integer numTestCases = Integer.parseInt(reader.readLine().trim());
+
+            while (numTestCases-- > 0) {
+                String input = reader.readLine() + "\n" + reader.readLine();
+
+                Data data = sample.parse(input);
+
+                String solution = sample.solve(data);
+
+                System.out.println(solution);
+
+            }
+
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+
     }
 
 }
--- src/test/java/com/ccbill/interview/WorkSampleTests.java
+++ src/test/java/com/ccbill/interview/WorkSampleTests.java
@@ -3,33 +3,111 @@ package com.ccbill.interview;
 import org.apache.log4j.Logger;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class WorkSampleTests {
+import java.io.*;
+
+public class WorkSampleTests extends WorkSample {
 
     private static final Logger logger = Logger.getLogger(WorkSampleTests.class);
 
     private WorkSample unitUnderTest;
 
+    /**
+     * Entire setup consists of just creating a new object for testing
+     */
     @Before
     public void setUp() {
         logger.info("setting up env....");
+
+        unitUnderTest = new WorkSample();
     }
 
+    /**
+     * @Test - first defined test case in the problem description
+     */
     @Test
     public void test001 () {
+        String input = "4 3\n-1 -3 4 2";
 
+        Data data = unitUnderTest.parse(input);
+
+        Assert.assertNotNull(data);
+
+        Assert.assertEquals(4, data.N);
+        Assert.assertEquals(3, data.K);
+
+        int[] expected = new int[] {-1, -3, 4, 2};
+
+        for (int i = 0; i < data.N; i++)
+            Assert.assertEquals(expected[i], data.arrival_times[i]);
     }
 
+    /**
+     * @Test - second defined test in sample output
+     */
     @Test
     public void test002 () {
+        String input = "4 2\n0 -1 2 1";
 
+        Data data = unitUnderTest.parse(input);
+
+        Assert.assertNotNull(data);
+
+        Assert.assertEquals(4, data.N);
+        Assert.assertEquals(2, data.K);
+
+        int[] expected = new int[] {0, -1, 2, 1};
+
+        for (int i = 0; i < data.N; i++)
+            Assert.assertEquals(expected[i], data.arrival_times[i]);
+    }
+
+    /**
+     * @Test - tests that the above tests execute correctly when put together and thrown at the main method (
+     * redirects input and output from stdin and stdout (respectively) and compares that against expected output)
+     */
+    @Test
+    public void test003() {
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        System.setIn(new BufferedInputStream(WorkSampleTests.class.getResourceAsStream("/test-003.txt")));
+        System.setOut(new PrintStream(output));
+
+        WorkSample.main(new String[]{});
+
+        try (BufferedReader reader = new BufferedReader(
+            new InputStreamReader(
+                WorkSampleTests.class.getResourceAsStream("/test-output-expected-003.txt")
+            )
+        )) {
+
+            StringBuffer buffer = new StringBuffer();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
+                buffer.append("\n");
+            }
+
+            String expectedOutput = buffer.toString();
+
+            Assert.assertTrue(expectedOutput.equals(output.toString()));
+
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
     }
 
     @After
     public void tearDown() {
         logger.info("tearing down....");
+
+            // Doesn't guarantee that gc will happen, but close enough for government work
+        unitUnderTest = null;
+        System.gc();
     }
 
 }
