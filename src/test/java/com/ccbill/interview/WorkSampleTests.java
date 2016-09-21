8d82fc83810abe81e22e52a7b2172439b2d04ef4
Robert Novak <me@robert-novak.com>
2016-09-21 10:58:33 -0700

    add test files

---
+++ src/test/java/com/ccbill/interview/WorkSampleTests.java
@@ -0,0 +1,22 @@
+package com.ccbill.interview;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class WorkSampleTests {
+
+    private WorkSample unitUnderTest;
+
+    @Before
+    public void setUp() {
+
+    }
+
+
+
+    @After
+    public void tearDown() {
+
+    }
+
+}
